3cb293aa8afc88cbe9357051619450818f5be624
bmcgrane302 <bmcgrane302@yahoo.com>
2018-01-24 14:49:54 -0700

    add income form

--- src/components/AddIncome.js
+++ src/components/AddIncome.js
@@ -5,7 +5,19 @@ import { Card, CardSection, Input } from './common';
 
 
 class AddIncome extends Component {
+  state = {
+    income_description: '',
+    income_budget: ''
+  }
+
+  handleSubmit = (e) => {
+     e.preventDefault()
+     console.log('work', this.state);
+   }
+
+
   render () {
+    console.log('add income', this.state);
     return (
       <Modal
         transparent
@@ -13,20 +25,51 @@ class AddIncome extends Component {
         onRequestClose={() => {}}
       >
         <View style={styles.containerStyle}>
-          <CardSection style={styles.cardSectionStyle}>
-            <Text style={styles.textStyle}>
-              INCOME
-            </Text>
-          </CardSection>
-          <CardSection>
-            <TouchableOpacity onPress={this.props.closeIncomeModal}>
-              <Text>
-                   CLOSE
-               </Text>
-            </TouchableOpacity>
-          </CardSection>
-        </View>
+          <Card>
+            <CardSection >
+              <Text style={styles.textStyle}>
+                INCOME
+              </Text>
+            </CardSection>
 
+            <CardSection>
+              <Input
+                label="Description"
+                placeholder="bill"
+                onChangeText={(income_description) => this.setState({income_description})}
+                value={this.props.income_description}
+              />
+            </CardSection>
+
+            <CardSection>
+              <Input
+                secureTextEntry
+                label="Budget"
+                placeholder="1000.00"
+                onChangeText={(income_budget) => this.setState({income_budget})}
+                value={this.props.income_budget}
+
+              />
+            </CardSection>
+
+            <CardSection>
+              <CardSection>
+                <TouchableOpacity onPress={this.handleSubmit}>
+                  <Text>
+                       ADD
+                   </Text>
+                </TouchableOpacity>
+              </CardSection>
+              <CardSection>
+                <TouchableOpacity onPress={this.props.closeIncomeModal}>
+                  <Text>
+                       CLOSE
+                   </Text>
+                </TouchableOpacity>
+              </CardSection>
+            </CardSection>
+          </Card>
+        </View>
       </Modal>
     )
   }
@@ -38,16 +81,17 @@ const styles = {
   },
   textStyle: {
     flex: 1,
-    fontSize: 18,
+    fontSize: 15,
     textAlign: 'center',
     lineHeight: 40
   },
   containerStyle: {
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    backgroundColor: 'rgba(0, 0, 0, 0.85)',
     position: 'relative',
     flex: 1,
     justifyContent: 'center'
-  }
+  },
+
 };
 
 export default AddIncome;
